90427e82a7b09f4c8d9e6e0e37ffe7fbe3ffa593
Tom <robot256@GMAIL.COM>
2020-04-25 21:21:16 -0400

    Version 0.18.1
    Added "disconnect-locomotive" control.

--- control.lua
+++ control.lua
@@ -8,29 +8,35 @@
 
 Position = require('__stdlib__/stdlib/area/position')
 
+
+-- global.player_data:
+--   rolling_stock 
+
 --== ON_INIT ==--
 -- Initialize global data tables
 function OnInit()
-
+  global.player_data = {}
 end
 script.on_init(OnInit)
 
 
 --== ON_CONFIGURATION_CHANGED ==--
 function OnConfigurationChanged()
-
+  global.player_data = global.player_data or {}
 end
 script.on_configuration_changed(OnConfigurationChanged)
 
 
---== ON_LUA_SHORTCUT ==--
+--== ON_EVENT("better-disconnect-train") ==--
+-- Replace the default disconnect behavior so that it prefers to uncouple an adjacent 
 function OnDisconnectTrain(event)
   -- Check if player is riding in train
   local player = game.players[event.player_index]
   local text_position = nil
+  local text_type = "tutorial-flying-text"
   if player then
     local text_position = {x=player.position.x, y=player.position.y}
-    local text = ""
+    local text = nil
     if player.vehicle and player.vehicle.valid then
       if player.vehicle.train then
         local wagon = player.vehicle
@@ -122,21 +128,106 @@ function OnDisconnectTrain(event)
             wagon.disconnect_rolling_stock(defines.rail_direction.front)
             text_position = front_text_position
           end
-          text = {"rolling-stock-disconnected"}
-          text_position.x = text_position.x - 1.4
+          
+          player.surface.create_entity({name = "better-disconnect-success-flying-text", position = text_position, 
+                                        text = {"rolling-stock-disconnected"}, render_player = player})
         else
           text = {"no-stock-to-disconnect-found"}
-          text_position.x = text_position.x - 2.2
         end
       else -- player.vehicle.train
         text = {"cant-disconnect-rolling-stock-not-in-rolling-stock"}
-        text_position.x = text_position.x - 3
       end
     else -- player.vehicle
       text = {"cant-disconnect-rolling-stock-not-in-vehicle"}
-      text_position.x = text_position.x - 2.5
     end
-    player.surface.create_entity({name = "flying-text", position = text_position, text = text, render_player = player})
+    
+    if text then
+      player.surface.create_entity({name = "better-disconnect-failure-flying-text", position = text_position, text = text, render_player = player})
+    end
   end -- player
 end
 script.on_event("better-disconnect-train", OnDisconnectTrain)
+
+
+--== ON_EVENT("better-disconnect-locomotive") ==--
+-- This control will only act if player is in a locomotive, and will disconnect the closest wagon
+function OnDisconnectLocomotive(event)
+  -- Check if player is riding in train
+  local player = game.players[event.player_index]
+  local text_position = nil
+  if player then
+    local text_position = {x=player.position.x, y=player.position.y}
+    local text = nil
+    if player.vehicle and player.vehicle.valid then
+      if player.vehicle.train then
+        if player.vehicle.type == "locomotive" then
+          local wagon = player.vehicle
+          local train = wagon.train
+          local wagon_pos = 0
+          -- Find what position of the train this wagon is
+          for i=1,#train.carriages do
+            if train.carriages[i].unit_number == wagon.unit_number then
+              wagon_pos = i
+              break
+            end
+          end
+          assert(wagon_pos > 0, "Could not find wagon in its own train!")
+          
+          -- Search front and back to find a wagon that is not a locomotive
+          local wagon1 = nil  -- dissimilar wagon to disconnect
+          local wagon1a = nil -- the closest wagon adjacent to it
+          
+          -- Find the first closest non-locomotive wagon and its nearest adjacent wagon
+          for i=1,#train.carriages do
+            if wagon_pos-i >= 1 then
+              if train.carriages[wagon_pos-i].type ~= "locomotive" then
+                wagon1 = train.carriages[wagon_pos-i]
+                wagon1a = train.carriages[wagon_pos-i+1]
+                break
+              end
+            end
+            if wagon_pos+i <= #train.carriages then
+              if train.carriages[wagon_pos+i].type ~= "locomotive" then
+                wagon1 = train.carriages[wagon_pos+i]
+                wagon1a = train.carriages[wagon_pos+i-1]
+                break
+              end
+            end
+          end
+          
+          -- Figure out which one is in front of this wagon.
+          -- Disconnect direction is based on orientation of wagon, not direction or ordering in train.
+          if wagon1 and wagon1.valid and wagon1a and wagon1a.valid then
+            -- Find angle to this wagon
+            local vector = {x=wagon1.position.x - wagon1a.position.x, y=wagon1.position.y - wagon1a.position.y}
+            local wagon_orientation = math.rad(wagon1a.orientation*360)
+            local rotated_vector = {x=(math.cos(wagon_orientation)*vector.x+math.sin(wagon_orientation)*vector.y), 
+                                    y=(math.sin(wagon_orientation)*vector.x-math.cos(wagon_orientation)*vector.y)}
+            text_position = Position.between(wagon1.position, wagon1a.position)
+            if rotated_vector.y > 1 then
+              wagon1a.disconnect_rolling_stock(defines.rail_direction.front)
+            elseif rotated_vector.y < -1 then
+              wagon1a.disconnect_rolling_stock(defines.rail_direction.back)
+            end
+            
+            player.surface.create_entity({name = "better-disconnect-success-flying-text", position = text_position,
+                                          text = {"rolling-stock-disconnected"}, render_player = player})
+          else
+            text = {"no-wagons-to-disconnect-found"}
+          end
+        else -- player.vehicle
+          text = {"cant-disconnect-wagons-not-in-locomotive"}
+        end
+      else -- player.vehicle.train
+        text = {"cant-disconnect-rolling-stock-not-in-rolling-stock"}
+      end
+    else -- player.vehicle
+      text = {"cant-disconnect-rolling-stock-not-in-vehicle"}
+    end
+    
+    if text then
+      player.surface.create_entity({name = "better-disconnect-failure-flying-text", position = text_position, text = text, render_player = player})
+    end
+  end -- player
+end
+script.on_event("better-disconnect-locomotive", OnDisconnectLocomotive)
--- data.lua
+++ data.lua
@@ -1,5 +1,5 @@
 --[[ Copyright (c) 2020 robot256 (MIT License)
- * Project: Vehicle Wagon 2 rewrite
+ * Project: Better Uncoupling Control
  * File: data.lua
  * Description:  Main Data Stage function.  Include all the prototype definitions.
  --]]
@@ -11,5 +11,26 @@ data:extend{
     key_sequence="",
     linked_game_control="disconnect-train",
     consuming="game-only",
-  }
+  },
+  {
+    type="custom-input",
+    name="better-disconnect-locomotive",
+    key_sequence="SHIFT + V",
+  },
+  {
+    type = "flying-text",
+    name = "better-disconnect-success-flying-text",
+    flags = {"not-on-map", "placeable-off-grid"},
+    time_to_live = 80,
+    speed = 0.04,
+    text_alignment = "center"
+  },
+  {
+    type = "flying-text",
+    name = "better-disconnect-failure-flying-text",
+    flags = {"not-on-map", "placeable-off-grid"},
+    time_to_live = 120,
+    speed = 0.031,
+    text_alignment = "center"
+  },
 }
